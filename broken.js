591fdd207dc1fa3927e3940cda281b4226a16e18
s2100688 <89017210+s2100688@users.noreply.github.com>
2022-02-16 12:49:24 +0200

    broken

---
+++ broken.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+const brokenSum = (a) => {
+  return a + b;
+};
+
+const minus = (a, b) => {
+  return a - b;
+};
+
+const brokeMinus = (b) => {
+  return b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+console.log(sum(2, 2)); // returns 4
+console.log(sum(2, 5)); // returns 7
+console.log(sum("a", "b")); // returns ab
